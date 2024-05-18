354dc76678ffffecefe5205eca09f25d0460fc36
Amadou001 <bamadou634@gmail.com>
2024-05-19 03:28:54 +0800

    Updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,7 +18,9 @@ import copy
 
 
 class FileStorage:
-    """File storage Class"""
+    """
+    File storage Class
+    """
     __file_path = "file.json"
     __objects = {}
 
@@ -28,7 +30,7 @@ class FileStorage:
         Returns:
             dictionnary: dictionnary representation of all objects
         """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """new: method to set in __objects the obj with key <obj class name>.id
@@ -37,14 +39,14 @@ class FileStorage:
             obj (object): instance of a class
         """
         key = "{}.{}".format(type(obj).__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """
         save: Method to save an object to
         a private class attribure __objects
         """
-        all_objects = copy.deepcopy(self.__objects)
+        all_objects = copy.deepcopy(FileStorage.__objects)
         for obj_id in all_objects.keys():
             all_objects[obj_id] = all_objects[obj_id].to_dict()
         for key, value in all_objects.items():
@@ -53,7 +55,7 @@ class FileStorage:
                     new_value = copy.deepcopy(value1)
                     f_value = new_value.isoformat()
                     all_objects[key][key1] = f_value
-        with open(self.__file_path, "w") as file:
+        with open(FileStorage.__file_path, "w") as file:
             json.dump(all_objects, file)
 
     def reload(self):
@@ -61,15 +63,15 @@ class FileStorage:
             reload: method to deserialise an object from an existing file
             then save it to private class attribute __object
         """
-        if os.path.isfile(self.__file_path):
+        if os.path.isfile(FileStorage.__file_path):
             try:
-                with open(self.__file_path, "r") as file:
+                with open(FileStorage.__file_path, "r") as file:
                     content = json.load(file)
                     for key, value in content.items():
                         class_name, obj_id = key.split('.')
                         cls = eval(class_name)
                         class_instance = cls(**value)
-                        self.__objects[key] = class_instance
+                        FileStorage.__objects[key] = class_instance
 
             except Exception:
                 pass
